6c94edf8b2cab9806573f2f17f723b45d474ecb6
sekineseishu <mika242401@gmail.com>
2024-08-26 18:37:48 +0900

    js修正

--- cmn/app.js
+++ cmn/app.js
@@ -3,9 +3,10 @@ function adjustForDPI() {
   
   // ノートPCのDPIスケーリングに合わせてサイズや位置を調整
   if (dpi == 1.25) {
+    document.querySelectorAll('.object').forEach(element => {
     Element.style.transform = 'scale(0.8)';
     Element.style.transformOrigin = 'top left';
-    
+    });
   }
 }
 
