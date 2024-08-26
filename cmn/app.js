37434a1c1f7c39cd557948f931869b98d5006f74
SekineSeishu <mika242401@gmail.com>
2024-08-26 18:35:48 +0900

    js

--- cmn/app.js
+++ cmn/app.js
@@ -2,8 +2,10 @@ function adjustForDPI() {
   const dpi = window.devicePixelRatio;
   
   // ノートPCのDPIスケーリングに合わせてサイズや位置を調整
-  if (dpi > 1) {
-      document.querySelector('.object').style.transform = `scale(${1 / dpi})`;
+  if (dpi == 1.25) {
+    Element.style.transform = 'scale(0.8)';
+    Element.style.transformOrigin = 'top left';
+    
   }
 }
 
